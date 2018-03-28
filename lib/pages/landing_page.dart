e5fdedf07dc199fe2e7abd01543694d702038367
BorhanUddin7420 <borhan7420@gmail.com>
2018-03-29 02:52:56 +0600

    Commit From Home

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -4,13 +4,15 @@ class LandingPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Material(
-      color: Colors.greenAccent,
+      color: Colors.redAccent,
       child: new InkWell(
+        onTap:()=> print("Hi k"),
+        onDoubleTap: ()=> print("DOuble Tap"),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text("Hello World"),
-            new Text("MkGhost")
+            new Text("Welcome To" , style: new TextStyle(color: Colors.lightGreen,fontSize: 50.0,),),
+            new Text("Quizz World!", style: new TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 40.0),)
           ],
         ),
       ),
